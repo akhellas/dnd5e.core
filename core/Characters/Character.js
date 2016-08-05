d501dfddb0fb074c3a7e20181225412ffcdba2c3
akhellas <ak.hellas@gmail.com>
2016-08-05 14:50:11 +0300

    structure

--- core/Characters/Character.js
+++ core/Characters/Character.js
@@ -1,6 +1,13 @@
 class Character {
     constructor() {
+        this._name = '';
+        this._xp = 0;
+        this._race = null;
+        this._classes = [];
+    }
 
+    addClass(characterClass) {
+        this._classes.push(characterClass);
     }
 }
 
--- core/Races/Race.js
+++ core/Races/Race.js
@@ -1,6 +1,20 @@
 class Race {
     constructor() {
+        this._abilities = {
 
+
+        };
+    }
+
+    static defaultAbilityModifiers() {
+        return {
+            Strength: 0,
+            Dexterity: 0,
+            Constitution: 0,
+            Intelligence: 0,
+            Wisdom: 0,
+            Charisma: 0
+        }
     }
 }
 
